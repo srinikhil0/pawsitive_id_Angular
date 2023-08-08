c113feaa131566c159c10dde430ea35e90bc22a7
Sri Nikhil Reddy <34449781+srinikhil99@users.noreply.github.com>
2023-08-08 12:55:16 -0400

    Update home.component.ts - bug fix

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,39 +10,73 @@ declare const showPreview: any;
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-
 export class HomeComponent {
   constructor(private http: HttpClient) {}
 
   @ViewChild('uploadBtn') uploadBtn!: ElementRef<HTMLInputElement>;
   fileChosen: string = 'No file chosen';
-  responseBreed: string | undefined;
+  responseBreed: string = '';
 
-  onchange(){
+  ngOnInit() {
+    // localStorage.clear(); // Clear local storage on page refresh
+    this.fetchImageFromStorage();
+    this.fetchResult();
+  }
+
+  onchange() {
     showPreview();
   }
 
   onFileChange(event: any): void {
     const fileList: FileList | null = event.target.files;
     if (fileList && fileList.length > 0) {
-      this.fileChosen = fileList[0].name;
-      this.onPhotoUploaded(event)
+      const file = fileList[0];
+      this.getBase64(file).then((base64Data: string) => {
+        localStorage.setItem('selectedImage', base64Data);
+        localStorage.setItem('selectedImageName', file.name); // Store the file name separately
+        this.fileChosen = file.name;
+        this.onPhotoUploaded(file);
+      });
     } else {
       this.fileChosen = 'No file chosen';
+      localStorage.removeItem('selectedImage');
+      localStorage.removeItem('selectedImageName'); // Remove the stored file name
+      localStorage.removeItem('predictedBreed');
     }
   }
-  onPhotoUploaded(event: any) {
-    const file = event.target.files[0];
+
+  fetchImageFromStorage() {
+    const selectedImageName = localStorage.getItem('selectedImageName');
+    if (selectedImageName) {
+      this.fileChosen = selectedImageName;
+      const selectedImage = localStorage.getItem('selectedImage');
+      if (selectedImage) {
+        const previewImg = document.getElementById('upload__file__bg') as HTMLImageElement;
+        previewImg.src = selectedImage;
+      }
+    }
+  }
+
+  getBase64(file: File): Promise<string> {
+    return new Promise<string>((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result as string);
+      reader.onerror = error => reject(error);
+    });
+  }
+
+  onPhotoUploaded(file: File) {
     const fileExtension = file.name.split('.').pop();
 
     if (fileExtension !== 'png' && fileExtension !== 'jpg' && fileExtension !== 'jpeg') {
-      alert("Please upload an image file in .png or .jpg format.");
+      this.setDefaultImage();
+      localStorage.removeItem('selectedImageName');
       return;
     } else {
       const formData = new FormData();
       formData.append('image', file);
-      this.responseBreed = 'Fetching Results...'
-
+      this.responseBreed = 'Fetching Results...';
 
       this.http.post('https://pawsitive-id-backend-2-k6kwv3hqka-uc.a.run.app', formData)
         .pipe(catchError(error => {
@@ -52,8 +86,23 @@ export class HomeComponent {
         .subscribe((response: any) => {
           // Handle response here
           this.responseBreed = response.predicted_class;
-          // console.log(response.predicted_class);
+          localStorage.setItem('predictedBreed', this.responseBreed);
         });
     }
+  }
+
+  setDefaultImage() {
+    alert("Please upload an image file in .png or .jpg format.");
+    const previewImg = document.getElementById('file-ip-1-preview') as HTMLImageElement;
+    previewImg.src = '../../assets/images/logo/home_page.png';
+  }
+
+  fetchResult() {
+    const result = localStorage.getItem('predictedBreed');
+    if (result) {
+      this.responseBreed = result;
     }
+  }
 }
+
+localStorage.clear();
